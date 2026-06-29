b62323477768e491812415dd059019b8c7eed739
Zsenpai <amhomed2345@gmail.com>
2026-06-29 17:47:47 +0200

    v2

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,27 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/ratelimiter.js";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 connectDB();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.listen(5001, () => {
   console.log("server started at port 5001");
 });
